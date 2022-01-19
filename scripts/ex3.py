4b67fa9e31dc700159f3b47b977cf397364b4725
Campbell Clarkson <campbell.clarkson@grad.moore.sc.edu>
2022-01-18 23:04:57 -0500

    Creating scripts folder

---
+++ scripts/ex3.py
@@ -0,0 +1,36 @@
+# Simple print below
+print("I will now count my chickens.")
+
+# Prints a string followed by the mathematical expression's solution
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100. - 25 * 3 % 4)
+
+# Simple string print
+print("Now I will count the eggs:")
+
+# Prints mathematical expression solution
+print(3. + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# Simple string print
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# Prints mathematical expression solution
+print(3 + 2 < 5 - 7)
+
+# Prints a string followed by the mathematical expression's solution
+print("What is 3 + 2?", 3. + 2)
+# Prints a string followed by the mathematical expression's solution
+print("What is 5 - 7?", 5. - 7)
+
+# Simple string print
+print("Oh, that's why it's False.")
+
+# Simple string print
+print("How about some more.")
+
+# Prints a string followed by the mathematical expression's solution
+print("Is it greater?", 5 > -2)
+# Prints a string followed by the mathematical expression's solution
+print("Is it greater or equal?", 5 >= -2)
+# Prints a string followed by the mathematical expression's solution
+print("Is it less or equal?", 5 <= -2)
