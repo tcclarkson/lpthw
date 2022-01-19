d4f20132e5a7d6b5b498dad3a1da6485fb3bdf2b
tcclarkson <campbell.clarkson@grad.moore.sc.edu>
2022-01-18 22:22:41 -0500

    Uploading exercises

---
+++ ex1.py
@@ -0,0 +1,9 @@
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print("Yay! Printing.")
+print("I'd much rather you 'not'")
+print('I "said" do not touch this.')
+print("I am printing this 'line' also.")
+# the 'octothorpe' makes a line into a comment and doesn't run the code on that line
